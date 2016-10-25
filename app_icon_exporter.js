fad0bb75235a7ad076707fd43af6a211cd5a0e87
Bradley Zellman <bradZellman@Bradleys-MacBook-Pro.local>
2016-10-24 22:58:40 -0500

    Add correct complete exporter file

--- app_icon_exporter.js
+++ app_icon_exporter.js
@@ -3,6 +3,42 @@ try
   // Prompt user to select iTunesArtwork file. Clicking "Cancel" returns null.
   var iTunesArtwork = File.openDialog("Select a sqaure PNG file that is at least 1024x1024.", "*.png", false);
 
+  var build_phone = false
+  var build_ipad = false
+  var build_android = false
+
+  var createTypeWindow = new Window("dialog");
+    createTypeWindow.alignChildren = "left";
+    var cb1 = createTypeWindow.add("checkbox",undefined, "iPhone Icons");
+    var cb2 = createTypeWindow.add("checkbox",undefined, "iPad Icons");
+    var cb3 = createTypeWindow.add("checkbox",undefined, "Android Icons");
+    
+    var build_icon_button = createTypeWindow.add("button", undefined, "Build Icons")
+    createTypeWindow.add("button", undefined, "Cancel")
+
+
+    build_icon_button.onClick = function(){
+      if(cb1.value == true) {
+        build_phone = true;
+      }
+
+      if (cb2.value == true) {
+        build_ipad = true;
+      }
+      
+      if (cb3.value == true) {
+        build_android = true;
+      } 
+
+      if (cb1 == false && cb2 == false && cb3 == false){
+        alert("No Icons Will be Created. Rerun the script to start again.")
+      }
+
+      createTypeWindow.close();
+    }
+
+    createTypeWindow.show();
+
   if (iTunesArtwork !== null) 
   { 
     var doc = open(iTunesArtwork, OpenDocumentType.PNG);
@@ -49,7 +85,28 @@ try
     sfw.transparency = true;
     doc.info = null;  // delete metadata
     
-    var icons = [
+    // var icons = [
+    //   {"name": "iTunesArtwork@2x", "size":1024},
+    //   {"name": "iTunesArtwork",    "size":512},
+    //   {"name": "Icon-60@2x",       "size":120},
+    //   {"name": "Icon@2x",          "size":114},
+    //   {"name": "Icon-Small-40@2x", "size":80},
+    //   {"name": "Icon-60",          "size":60},
+    //   {"name": "Icon",             "size":57},
+    //   {"name": "Icon-Small@2x",    "size":58},
+    //   {"name": "Icon-Small-40",    "size":40},
+    //   {"name": "Icon-Small",       "size":29},
+    //   {"name": "Icon-50",          "size":50},
+    //   {"name": "Icon-50@2x",       "size":100},
+    //   {"name": "Icon-72",          "size":72},
+    //   {"name": "Icon-72@2x",       "size":144},
+    //   {"name": "Icon-76",          "size":76},
+    //   {"name": "Icon-76@2x",       "size":152},
+    // ];
+
+    var icons = [];
+
+    var iPhone_icons = [
       {"name": "iTunesArtwork@2x", "size":1024},
       {"name": "iTunesArtwork",    "size":512},
       {"name": "Icon-60@2x",       "size":120},
@@ -60,8 +117,37 @@ try
       {"name": "Icon-Small@2x",    "size":58},
       {"name": "Icon-Small-40",    "size":40},
       {"name": "Icon-Small",       "size":29},
-      
     ];
+
+    var iPad_icons = [
+      {"name": "Icon-50",          "size":50},
+      {"name": "Icon-50@2x",       "size":100},
+      {"name": "Icon-72",          "size":72},
+      {"name": "Icon-72@2x",       "size":144},
+      {"name": "Icon-76",          "size":76},
+      {"name": "Icon-76@2x",       "size":152},
+    ];
+
+    var android_icons = [
+      {"name": "hires",                   "size":512},
+      {"name": "ic_launcher96by96.png",   "size":96},
+      {"name": "ic_launcher72by72.png",   "size":72},
+      {"name": "ic_launcher48by48.png",   "size":48},
+      {"name": "ic_launcher36by36.png",   "size":36},
+    ];
+
+    if (build_phone == true){
+      icons = icons.concat(iPhone_icons)
+    }
+
+    if (build_ipad == true){
+      icons = icons.concat(iPad_icons)
+    }
+
+    if (build_android == true){
+      icons = icons.concat(android_icons)
+    }
+
 
     var icon;
     for (i = 0; i < icons.length; i++) 
@@ -82,7 +168,7 @@ try
       doc.activeHistoryState = startState; // undo resize
     }
 
-    alert("iOS Icons created!");
+    alert("Your Icons have been created!");
   }
 }
 catch (exception)
